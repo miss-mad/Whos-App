ebc936a648ca8f7f14f7892ca0fb3e6c68e76a21
git geek <cout.1.endl@gmail.com>
2022-10-17 13:08:25 -0400

    changed routes need to test with handlebar pg

--- controllers/api/messageRoutes.js
+++ controllers/api/messageRoutes.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-const { Contact } = require("../../models");
+const { Message } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.get("/:id", withAuth, async (req, res) => {
   try {
-    const newContact = await Contact.findOne({
+    const newMessage = await Message.findOne({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    res.status(200).json(newContact);
+    res.status(200).json(newMessage);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -19,12 +19,12 @@ router.get("/:id", withAuth, async (req, res) => {
 
 router.post("/", withAuth, async (req, res) => {
   try {
-    const newContact = await Contact.create({
+    const newMessage = await Message.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newContact);
+    res.status(200).json(newMessage);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -32,19 +32,19 @@ router.post("/", withAuth, async (req, res) => {
 
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const contactData = await Contact.destroy({
+    const messageData = await Message.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!contactData) {
-      res.status(404).json({ message: "No contact found with this id!" });
+    if (!messageData) {
+      res.status(404).json({ message: "No chat found with this id!" });
       return;
     }
 
-    res.status(200).json(contactData);
+    res.status(200).json(messageData);
   } catch (err) {
     res.status(500).json(err);
   }
--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,49 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
+const loginFormHandler = async (event) => {
+  event.preventDefault();
 
-router.use("/api", apiRoutes);
+  // Collect values from the login form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-router.use((req, res) => {
-  res.send("<h1>404</h1>");
-});
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-module.exports = router;
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace("/contacts");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/contacts");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);

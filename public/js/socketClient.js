edbe03926db82cd7a04c5c005b671a98f8557c77
miss-mad <mmlh.harris@gmail.com>
2022-10-16 21:14:12 -0400

    getting users and messages to print on client side

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -8,3 +8,35 @@ const messages = document.getElementById("messages");
 const messageForm = document.getElementById("messageForm");
 const roomName = document.getElementById("rooms");
 const userList = document.getElementById("users");
+
+socket.emit("joinRoom", { username, room });
+
+const handleSocket = () => {
+  socket.on("roomUsers", (event) => {
+    const listItem = document.createElement("li");
+    const message = event.target.elements.input.value.trim();
+
+    listItem.setAttribute("class", "chat");
+    listItem.textContent = "user: " + message;
+    messages.appendChild(listItem);
+
+    allMessages.push(message);
+    console.log(allMessages);
+
+    input.value = "";
+    input.focus();
+
+    userList.innerHTML = "";
+    users.forEach((user) => {
+      const li = document.createElement("li");
+      li.innerText = user.username;
+      userList.appendChild(li);
+    });
+  });
+
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+};
+
+handleSocket();

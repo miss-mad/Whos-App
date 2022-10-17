31b277296555b74e1b76f76306465b2d2e645375
miss-mad <mmlh.harris@gmail.com>
2022-10-16 20:52:38 -0400

    socket event: user joins a room

--- socketServer.js
+++ socketServer.js
@@ -1,11 +1,36 @@
 // 3 basic socket events:
-    // user joins a room
-    // user sends a message
-    // user leaves a room
+// user joins a room
+// user sends a message
+// user leaves a room
 
-const formatMessage = require("./utils/socketHelpers")
+const formatMessage = require("./utils/socketHelpers");
 
+const socketio = require("socket.io");
+const io = socketio(server);
 
-module.exports = {
-    run_socket: run_socket,
-  };
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    console.log("user is in a room: ", username, room, socket.id);
+
+    const user = addUser(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage("Good Bot ", "Who's App!"));
+
+    socket.emit("onJoinRoom", "user has joined the room");
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage("Good Bot ", `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+});
+
--- utils/socketHelpers.js
+++ utils/socketHelpers.js
@@ -1,17 +1,31 @@
 // all helper functions for socket.io
-const moment = require('moment');
+const moment = require("moment");
 
 // for formatting the chat message to add the user's username and time stamp with MomentJS
 const formatMessage = (username, text) => {
   return {
     username,
     text,
-    time: moment().format('h:mm a')
+    time: moment().format("h:mm a"),
   };
-}
+};
 
+// generating a random user id for every single person who joins the server - this is to test socket, will replace with user's username
 const generate_random_user_id = () => {
-  return Math.floor(Math.random() * 100)
-}
+  return Math.floor(Math.random() * 100);
+};
 
-module.exports = formatMessage, generate_random_user_id;
+// the following functions are all for socketServer.js to help socket's events: user enters a room, user sends a message, and user leaves a room
+const addUser = (id, username, room) => {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+};
+
+const getRoomUsers = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+(module.exports = formatMessage), generate_random_user_id;

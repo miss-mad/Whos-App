5a88868d9bd54a07ae88f91bf95aa42a473baba5
miss-mad <mmlh.harris@gmail.com>
2022-10-16 20:45:51 -0400

    socket handlebars pages + added helper js function

--- utils/socketHelpers.js
+++ utils/socketHelpers.js
@@ -2,7 +2,7 @@
 const moment = require('moment');
 
 // for formatting the chat message to add the user's username and time stamp with MomentJS
-function formatMessage(username, text) {
+const formatMessage = (username, text) => {
   return {
     username,
     text,
@@ -10,4 +10,8 @@ function formatMessage(username, text) {
   };
 }
 
-module.exports = formatMessage;
+const generate_random_user_id = () => {
+  return Math.floor(Math.random() * 100)
+}
+
+module.exports = formatMessage, generate_random_user_id;

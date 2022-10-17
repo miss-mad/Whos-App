6a867ec44fc32791d7d4c2c3b6f6e49c49574bd0
Hunter O'Neal <hunteroneal54@gmail.com>
2022-10-17 16:59:30 -0500

    added packages, worked on Contact.js model

--- models/Contact.js
+++ models/Contact.js
@@ -3,26 +3,13 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      
-    }
-  }
+  class Contact extends Model {}
+  
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING
+    userName: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Contact',
   });
-  return User;
+  return Contact;
 };

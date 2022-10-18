d4198520714f64fdb65103f6701426fd7867cd1b
miss-mad <mmlh.harris@gmail.com>
2022-10-18 15:58:53 -0400

    adjusted sockethomeroute to render user_id in url

--- controllers/socketHomeRoutes.js
+++ controllers/socketHomeRoutes.js
@@ -3,10 +3,27 @@
 const router = require("express").Router();
 
 // all chats page - route must match name of the page on main.handlebars
+// INSERT WITHAUTH later once we know this is working
 router.get("/allchats", async (req, res) => {
-    // name must match handlebars file
-    res.render("socketAllChats");
+  console.log("req.session", req.session)
+  // finds user by its primary key and gets only the user's username data
+  const userData = await User.findByPk(req.session.user_id, {
+    attributes: { include: ["username"] },
   });
+
+  console.log("\nuserData", userData)
+
+  let tempUserData = userData.get({ plain: true });
+
+  // render name must match handlebars file
+  // then renders username data with the socketAllChats handlebars page
+  res.render("socketAllChats", {
+    ...tempUserData,
+    // comment this back in once logged in feature is working
+    // logged_in: req.session.logged_in,
+  });
+  // now the username is included in the URL when the user clicks a room to join
+});
   
   // single chat page - route must match name of the page on socketAllChats.handlebars
   router.get("/user/:user_id/room/:room_id", async (req, res) => {

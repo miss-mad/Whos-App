e88fe513f2ab9d0b33dd9b4ae582dfc4f5b1cddf
miss-mad <mmlh.harris@gmail.com>
2022-10-18 16:20:17 -0400

    testing seeds

--- seeders/faker-seed.js
+++ seeders/faker-seed.js
@@ -1,20 +1,24 @@
-const faker = require('faker');
-const users = [...Array(100)].map((user) => (
-  {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    email: faker.internet.email(),
-    userName: faker.internet.userName(),
-    password: faker.internet.password(8),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-))
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('User', users, {});
+const faker = require("faker");
+const { User } = require("../models");
+const users = [...Array(100)].map((user) => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  userName: faker.internet.userName(),
+  password: faker.internet.password(8),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
+
+const seedUsers = () => User.bulkCreate(users);
+
+module.exports =
+  ({
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert("User", users, {});
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete("User", null, {});
+    },
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('User', null, {});
-  }
-};
+  seedUsers);
---
+++ seeders/seeds.js
@@ -0,0 +1,20 @@
+// Import seeds.js file
+const seedUsers = require("./user-seeds");
+
+// Use the sequelize connection from the connection.js file
+const sequelize = require("../config/connection");
+
+// Function expression to seed all tables using async/await, then logging a message in the terminal once done
+// Must be in this order because otherwise will throw an error because certain foreign keys being referenced don't exist yet
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+
+  await seedUsers();
+  console.log("\n----- USERS SEEDED -----\n");
+
+  process.exit(0);
+};
+
+// To start seeding function
+seedAll();

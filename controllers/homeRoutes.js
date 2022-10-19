783894ae2313d214dfe14abbe15ac04db9519043
git geek <cout.1.endl@gmail.com>
2022-10-19 15:04:45 -0400

    contacts handlebar page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,33 +3,33 @@ const { Contact, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 // ask for login
-router.get("/", async (req, res) => {
-  try {
-    // force login
-    res.redirect("/login");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", async (req, res) => {
+//   try {
+//     // force login
+//     res.redirect("/login");
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/login", (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect("/profile");
+//     return;
+//   }
+
+//   res.render("login");
+// });
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect("/profile");
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect("/profile");
+  //   return;
+  // }
 
-  res.render("login");
-});
-
-router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect("/profile");
-    return;
-  }
-
-  res.render("login");
+  res.render("socketAllChats");
 });
 
 // PROFILE ROUTES
